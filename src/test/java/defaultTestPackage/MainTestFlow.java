b899a0bdcd33f05be3a563fefade2a7dc44b42c0
Vignesh Manikandan <vigneshmk.2258@gmail.com>
2026-06-24 10:54:12 +0530

    Main Test Flow created

---
+++ src/test/java/defaultTestPackage/MainTestFlow.java
@@ -0,0 +1,30 @@
+package defaultTestPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class MainTestFlow {
+
+	WebDriver driver;
+	
+	@BeforeTest
+	public void SetupTest()
+	{
+		
+	}
+	
+	@Test
+	public void StartTest()
+	{
+		
+	}
+	
+	@AfterTest
+	public void EndTest()
+	{
+		
+	}
+
+}
