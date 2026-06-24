1beac4842169b0bd0c00359a04ec513064d778da
Vignesh Manikandan <vigneshmk.2258@gmail.com>
2026-06-24 10:51:37 +0530

    Universal Properties added

---
+++ src/test/java/configPackage/UniversalProperties.java
@@ -0,0 +1,36 @@
+package configPackage;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class UniversalProperties {
+
+	public static String GetProperty(String propertyIndex)
+	{
+		String value = "default";
+		
+		Properties props = new Properties();
+		
+		String userDir = System.getProperty("user.dir");
+		
+		String cfgDir = userDir + "/src/test/java/configPackage/config.properties";
+		
+		try
+		{
+			FileInputStream input = new FileInputStream(cfgDir);
+			props.load(input);
+			
+			props.getProperty(propertyIndex);
+			
+		}
+		catch(Exception e)
+		{
+			e.getMessage();
+			e.getCause();
+			e.printStackTrace();
+		}
+		
+		return value;
+	}
+
+}
