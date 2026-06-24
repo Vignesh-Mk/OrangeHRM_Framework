de6bf817a7163eb1892358674f12aafb40077d22
Vignesh Manikandan <vigneshmk.2258@gmail.com>
2026-06-24 10:57:58 +0530

    MainTestFlow Backup 1

--- src/test/java/defaultTestPackage/MainTestFlow.java
+++ src/test/java/defaultTestPackage/MainTestFlow.java
@@ -1,9 +1,14 @@
 package defaultTestPackage;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import configPackage.UniversalProperties;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class MainTestFlow {
 
@@ -12,19 +17,44 @@ public class MainTestFlow {
 	@BeforeTest
 	public void SetupTest()
 	{
-		
+		SetupBrowserDrivers();
 	}
 	
 	@Test
 	public void StartTest()
 	{
+		String targetURL = UniversalProperties.GetProperty("targetURL");
 		
+		driver.get(targetURL);
 	}
 	
 	@AfterTest
 	public void EndTest()
 	{
 		
+	}
+	
+	private void SetupBrowserDrivers()
+	{
+		String browser = UniversalProperties.GetProperty("browser");
+		
+		switch(browser)
+		{
+			case "default":
+				WebDriverManager.firefoxdriver().setup();
+				driver = new FirefoxDriver();
+				break;
+				
+			case "firefox":
+				WebDriverManager.firefoxdriver().setup();
+				driver = new FirefoxDriver();
+				break;
+				
+			case "chrome":
+				WebDriverManager.chromedriver().setup();
+				driver = new ChromeDriver();
+				break;
+		}
 	}
 
 }
